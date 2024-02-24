2245a2bcfa68f7bb3dd498cdf06d5dc8e8d94a34
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-02-24 19:18:33 +0530

    Commit - 02
    Adding Signup Logic

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -9,23 +9,59 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const EnteredEmail = emailInputRef.current.value;
+    const EnteredPassword = passwordInputRef.current.value;
+
+    if(isLogin) {
+
+    } else {
+      fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAXIcW9GzBV7V6fjJxeMwJEP-_TnRcP4CA` ,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: EnteredEmail,
+            password: EnteredPassword,
+            returnSecureToken: true
+          }),
+          headers: {'Content-Type': 'application/json'}
+        }
+      ).then(res => {
+        if(res.ok){
+          //....
+          return res.json().then(data => {
+            console.log(data);
+          })
+        } else {
+          return res.json().then (data => {
+            //Show Error Modal
+            console.log(data);
+          });
+        }
+      });
+    }
+  }
+
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form >
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input
-            type='password'
-            id='password'
-            required
-          />
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
+          <button>
+            { isLogin ? 'Login' : 'Create Account' }
+          </button>
           <button
             type='button'
             className={classes.toggle}
